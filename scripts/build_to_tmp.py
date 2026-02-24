03f893c6ab5507fb1915041a7655cd9fe7291e9e
Thomas Aglassinger <roskakori@users.sourceforge.net>
2026-02-24 08:03:18 +0100

    #2 Migrate to uv

---
+++ scripts/build_to_tmp.py
@@ -0,0 +1,19 @@
+import logging
+import shutil
+import subprocess
+from pathlib import Path
+
+TEMP_FOLDER = Path("/tmp/roskakori.at/")
+_log = logging.getLogger("build")
+
+if __name__ == "__main__":
+    shutil.rmtree(TEMP_FOLDER)
+    subprocess.run([
+        "uv",
+        "run",
+        "lektor",
+        "build",
+        "--output-path",
+        str(TEMP_FOLDER.resolve()),
+    ])
+    _log.info("✅ The built files are located in %s.", str(TEMP_FOLDER))
